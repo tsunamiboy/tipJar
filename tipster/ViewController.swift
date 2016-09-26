aa44d5566230f15432de35d87f4e92631f41dc8e
David Blake <dvdblake63@gmail.com>
2016-09-26 15:27:41 -0700

    Implemented picker wheel. Issues: defaults improperly set and Data does not update until new Data is entered. Explore "viewwillload"

--- tipster/ViewController.swift
+++ tipster/ViewController.swift
@@ -8,15 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-   
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var tipSelection: UIPickerView!
     @IBOutlet weak var billedField: UITextField!
-    @IBOutlet weak var tipSelector: UISegmentedControl!
+//    @IBOutlet weak var tipSelector: UISegmentedControl!
     @IBOutlet weak var tipCalculatedLabel: UILabel!
     @IBOutlet weak var totalCalculatedLabel: UILabel!
     @IBOutlet weak var percentageLabel: UILabel!
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tipDbls.count
+    }
     
-    let tipArray = [0.10, 0.15, 0.2]
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(tipDbls[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        tipFactor = tipDbls[row]
+        tipLabel = tipStr[row]
+    }
+    
+    let tipDbls = [0.1,0.12,0.15,0.18,0.2,0.22]
+    let tipStr = [".1",".12",".15",".18",".2",".22"]
+    var tipFactor: Double = 0
+    var tipLabel: String = "eh"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +50,15 @@ class ViewController: UIViewController {
     }
     
     @IBAction func billedAmount(_ sender: AnyObject) {
+        
         let amountOwed = Double(billedField.text!) ?? 0
-        let calculatedTip = amountOwed * tipArray[tipSelector.selectedSegmentIndex]
+        print("tipFactor is: \(tipFactor)")
+        let calculatedTip = amountOwed * tipFactor
         let calculatedTotal = amountOwed + calculatedTip
-        
-        percentageLabel.text = "\(tipArray[tipSelector.selectedSegmentIndex])"
+        percentageLabel.text = tipLabel
         tipCalculatedLabel.text = String(format: "$%.2f", calculatedTip)
-//        print("calculated Tip = " + String(format: "$%.2f", calculatedTip))
+        print("calculated Tip = " + String(format: "$%.2f", calculatedTip))
         totalCalculatedLabel.text = String(format: "$%.2f", calculatedTotal)
-        
     }
     
     @IBAction func onTap(_ sender: AnyObject) {
